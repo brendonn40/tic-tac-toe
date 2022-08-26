d56004eb112f87d8296e9e6bae0e328bef829ce6
Brendon-note <brendoncesa40@gmail.com>
2022-08-25 22:25:01 -0300

    makemove funciton + players object

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 const gameboard = (function(){
-    let board =["x","o","x","x","o","x","x","o","x"]
+    let board =["x","o","x","x","o","x","x","o",""]
+    const makeMove = (player,position) =>{
+        if(board[position] === ""){
+            board[position] = player.marker
+        }
+    }
 
     const getBoard = () =>{
         return board
     }
-    return{getBoard}
+    return{getBoard,makeMove}
 })()
 
+const playerFactory = (marker) =>{
+    return {marker}
+}
 
 const displayController = (function(){
     const display = document.getElementById("display")
     const createBoardDisplay = (board) => {
         for (let index = 0; index < board.length; index++) {
             let play = document.createElement("button")
+            play.textContent= board[index]
             display.appendChild(play)
         }
     }
     return{createBoardDisplay}
 })()
+// brendon = playerFactory("o")
+// gameboard.makeMove(brendon,8)
 console.log(gameboard.getBoard())
 displayController.createBoardDisplay(gameboard.getBoard())
